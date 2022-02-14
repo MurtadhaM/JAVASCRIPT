6061feedfc5a4d90275b8340d8c3ab3bcd37bc7a
Murtadha Marzouq <45076915+MurtadhaM@users.noreply.github.com>
2022-02-14 04:15:12 -0500

    Update 2.0 🦋🧠

---
+++ Math/P5/Boolean.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.10
+from pyeda.inter import *
+
+print(int(False))
+
+print(list(iter_points([x, y])))
---
+++ Math/P5/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(200, 400);
+
+
+}
+
+function draw() {
+  background(220);
+     line(20 )
+  stroke(0);
+}
