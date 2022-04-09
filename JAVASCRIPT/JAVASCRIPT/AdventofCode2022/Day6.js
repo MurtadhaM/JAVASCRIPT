f46f89225d86e9aa19b289a6f81a09d306a66d51
Murtadha Marzouq <45076915+MurtadhaM@users.noreply.github.com>
2022-04-09 06:45:07 -0400

    💢

--- JAVASCRIPT/JAVASCRIPT/AdventofCode2022/Day6.js
+++ JAVASCRIPT/JAVASCRIPT/AdventofCode2022/Day6.js
@@ -1,54 +1,83 @@
 const { readFileSync, readFileAsync } = require('fs')
-
+let days =256;
 let fish = {
-  id: [],
-  remaining: () => {
-    // iterate over the array of fish
+  starterFish: [],
+  fish: [],
 
-    let num = fish.id.length
+      
+    
 
-    for (let i = 0; i < num; i++) {
-        if(fish.id[i] == fish.id[i - 1]){
-            fish.id.splice(i, 1)
-            con
-        }
-      // setting up the logic for the rules
-      if (fish.id[i] == 0) {
-        // resetting the fish
-       fish.id[i] = 6
-        // adding another fish
-        fish.id.push(8)
-      } else {
-        // incrementing  the fish value
-        fish.id[i] = fish.id[i] - 1
-      }
-    }
-    return fish.id;
-  },
+  
+   
+    
 }
 
 readFileSync('input.txt', 'utf8')
   .split(',')
   .forEach((character) => {
-    fish.id.push(character)
+    fish.starterFish.push(character)
   })
-  const delay = ms => new Promise(res => setTimeout(res, ms));
 
-  const Process_Data_Async  = async () => {
-  // settup the number of days 80 for part 1 and part 2 is 256
-let numberOfDays = 80;  
-console.log('Initial state: ', fish.id)
-for (let i = 0; i < numberOfDays; i++) {
-    
-    fish.id = fish.remaining();
-    console.log(fish.id)
-}
+  let day = [];  
+
+const valuesUpdate = (f) => {
+  var textFish = [];
   
-     
-    console.log("After " + numberOfDays+ ' Days ', fish.id.length) 
+  if (f.length == 0 || days <= 1) {
+  
+    return f;
+  }
 
+  fish.starterFish = f.toString().replace(/0/g, '7,9').replace(/'7'/g,8).replace(/'9'/g,9).replace(/1/g, 0).replace(/2/g, 1).replace(/3/g, 2).replace(/4/g, 3).replace(/5/g, 4).replace(/6/g, 5).replace(/7/g, 6).replace(/8/g, 7).replace(/9/g, 8)
+  textFish = fish.starterFish.split(',');
+  if (typeof f === 'string') {
+    
+
+   return valuesUpdate(f.split(','));
   }
 
 
 
-  Process_Data_Async()
+
+  return textFish;
+}
+
+const processDay = () => {
+  for (let i = 0; i < days; i++) {
+
+    
+    
+    let DayResults = valuesUpdate(fish.starterFish);
+
+    console.log('Day: ' + (i+1) + ' : ' + DayResults.length)
+    
+
+  }
+  // for (let i = 0; i < fish.starterFish.length    ; i++) {
+
+  //   if((parseInt(fish.starterFish[i])    )  == 0 ){
+  //     fish.starterFish.push(9)
+  //     fish.starterFish[i] = 7 ;
+
+     
+  //   }
+  //   fish.starterFish[i] = fish.starterFish[i] - 1;
+
+
+
+  // }
+return fish.fish.length;
+}
+
+let values = 0;
+fish.starterFish = fish.starterFish.toString().split(',');
+console.log('Initial state: ' + fish.starterFish);
+console.log(processDay())
+// const used = process.memoryUsage().heapUsed / 1024 / 1024;
+// console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+
+//  processDay()
+
+ 
+
+
---
+++ JAVASCRIPT/JAVASCRIPT/AdventofCode2022/bruh.js
@@ -0,0 +1,7 @@
+
+let x = [1,2,3,4,5,"Bruh"];
+
+for (let i = 0; i < x.length; i++) {
+  console.log(x[i])
+}
+console.log(x[5]);
