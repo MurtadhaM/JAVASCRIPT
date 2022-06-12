19b8305462038322e1247a75fda6762bf13e7ad4
Murtadha Marzouq <mmarzou0@gmail.com>
2022-06-12 13:27:47 -0400

    Ⓜ️

--- Math/P5/p5.js
+++ Math/P5/p5.js
@@ -5,52 +5,45 @@
  * variables set the positions based on the value of 'd'.
  */
 
-let width = 400;
-let height = 400;
+let width = 400
+let height = 400
 function setup() {
-
   // Sets the screen to be 720 pixels wide and 400 pixels high
-  createCanvas(width, height, WEBGL);
-  background(0);
- // Make the points 10 pixels in size
- strokeWeight(5);
+  createCanvas(width, height, WEBGL)
+  background(0)
+  // Make the points 10 pixels in size
+  strokeWeight(5)
+  stroke(255)
 }
 
 // Contains the elements to draw
 function start() {
-  let points = createPoints(20,.5);
-  return points;
-
+  let points = createPoints(20, 0.5)
+  return points
 }
 
-
-
-
-function createPoints(number, size ){
-  let radius = 360/(number);
-  console.log(radius);
-  offset = 30;
+function createPoints(number, size) {
+  let radius = 360 / number
+  console.log(radius)
+  offset = 30
   let points = []
 
-  let distance = (width/2) - offset;
-   distance = distance * size;
+  let distance = width / 2 - offset
+  distance = distance * size
   // the distance between the points
-  console.log(distance);
-  for(let i = 0; i <= number; i++){
+  console.log(distance)
+  for (let i = 0; i <= number; i++) {
     // draw an arc from the center of the screen
-    let x = width/2 + cos(radians(i*radius))*distance;
-    let y = height/2 + sin(radians(i*radius))*distance;
-    let point1 =  point(x,y);
-    points.push(point1);
+    let x = width / 2 + cos(radians(i * radius)) * distance
+    let y = height / 2 + sin(radians(i * radius)) * distance
+    let point1 = point(x, y)
+    points.push(point1)
   }
 
-  
   console.log(points.length)
-  return points;
+  return points
 }
 
-
-function calculateLocation(point){
- 
-  return point;
+function calculateLocation(point) {
+  return point
 }
---
+++ OCR_Crack/HostsAdd.bat
@@ -0,0 +1,3 @@
+echo "127.0.0.1 pdfconverters.net" >> c:\windows\system32\drivers\etc\hosts
+echo "127.0.0.1 easyscreenocr.com" >> c:\windows\system32\drivers\etc\hosts
+start "https://easyscreenocr.com/how-to-ocr-screenshot-on-windows/"
