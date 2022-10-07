b208c1c87f2d8a7465fcf0c1e8d87ab3a455a584
Murtadha Marzouq <mmarzou0@gmail.com>
2022-10-06 20:06:40 -0400

    😜

--- OCR_Crack/app.js
+++ OCR_Crack/app.js
@@ -18,15 +18,22 @@ const app = express();
     var mine = JSON.parse('   {"success": "Exceeded maximum number of activations",        "code": "103",        "activated": true,        "timestamp": 1645379846,        "sig": "ffe67b2cc95cb9f87d5e3b29ee080a3f"}')
 
     console.log(request.method + ' ' + request.url);
+    response.writeHead(200, {'Content-Type': 'application/json'});
     response.end(JSON.stringify(mine));
     });
+
     server2.on('request', function(request, response) {
         var mine = JSON.parse('   {"success": "Exceeded maximum number of activations",        "code": "103",        "activated": true,        "timestamp": 1645379846,        "sig": "ffe67b2cc95cb9f87d5e3b29ee080a3f"}')
-    
+
         console.log(request.method + ' ' + request.url);
+        response.writeHead(200, {'Content-Type': 'application/json'});
+
+
         response.end(JSON.stringify(mine));
+        
         });
     }
+ 
     catch(err){
         console.log(err);
     }
@@ -37,4 +44,3 @@ const app = express();
 // Fucking worked!!!!
 console.log('HTTPS Server listening on localhost:%s', 443);
 console.log('HTTPS Server listening on localhost:%s', 80);
-
